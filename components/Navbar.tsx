3706425257491f8444af96436f9d58b198eab4f1
Nnamdi Okeke <onnamdialex@gmail.com>
2024-02-07 16:57:28 +0100

    :construction: Wip...

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,37 @@ import React from "react";
 import Image from "next/image";
 import logo from "../public/logo.png";
 import styles from "../styles/navbar.module.css";
+import Link from "next/link";
+
+const Navlinks = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="">Products/Services</Link>
+        </li>
+        <li>
+          <Link href="">Career Opportunities</Link>
+        </li>
+        <li>
+          <Link href="">Whitepaper</Link>
+        </li>
+        <li>
+          <Link href="">Legal Pages</Link>
+        </li>
+        <li>
+          <Link href="">About Us</Link>
+        </li>
+        <li>
+          <Link href="">Blog</Link>
+        </li>
+        <li>
+          <Link href="">FAQs</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 const Navbar = () => {
   return (
@@ -9,6 +40,9 @@ const Navbar = () => {
       <div className={styles.navbar}>
         <div>
           <Image src={logo} alt="logo" />
+        </div>
+        <div>
+          <Navlinks />
         </div>
       </div>
     </div>
