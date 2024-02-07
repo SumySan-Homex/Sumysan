5de0605795e1add2c55e4f43a9330ae3b60f2663
Nnamdi Okeke <onnamdialex@gmail.com>
2024-02-07 16:44:55 +0100

    :construction: Wip ...

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Image from "next/image";
+import logo from "../public/logo.png";
+import styles from "../styles/navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className={styles.navbar}>
+        <div>
+          <Image src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
