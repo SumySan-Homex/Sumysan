cf659c66b5da454a4d878e93df9b18392f38809d
Nnamdi Okeke <onnamdialex@gmail.com>
2024-02-08 19:05:03 +0100

    :art: Improve structure. outline of main card. work still in progress

--- components/About.tsx
+++ components/About.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import Image from "next/image";
 import re1 from "../public/re1.png";
 import re2 from "../public/re2.png";
+import styles from "../styles/about.module.css";
 
 const About = () => {
   return (
     <div>
-      <div>
-        <Image src={re1} alt="estate" />
-        <Image src={re2} alt="house" />
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <h2>Card Title</h2>
+          <p>Card Content</p>
+        </div>
+
+        <div className={styles.images}>
+          <Image src={re1} alt="estate" />
+          <Image src={re2} alt="house" />
+        </div>
       </div>
     </div>
   );
