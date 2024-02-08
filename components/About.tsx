81946ec4899805b6dd3590b6d0e15d05724f6d8a
Nnamdi Okeke <onnamdialex@gmail.com>
2024-02-08 17:09:38 +0100

    :sparkles: Intro new features/images for the abt sec. wip...

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Image from "next/image";
+import re1 from "../public/re1.png";
+import re2 from "../public/re2.png";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div>
+        <Image src={re1} alt="estate" />
+        <Image src={re2} alt="house" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
