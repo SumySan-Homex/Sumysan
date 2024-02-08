fc9e26cc089df13865bfbd02bfd986249110dcbd
Nnamdi Okeke <onnamdialex@gmail.com>
2024-02-08 16:57:13 +0100

    :sparkles: Start of the about section. wip ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import About from "@/components/About";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <About />
     </div>
   );
 };
