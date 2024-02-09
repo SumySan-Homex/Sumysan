2c04f0fb895ca5a4cad232ffba5b72ef440291c0
Nnamdi Okeke <onnamdialex@gmail.com>
2024-02-09 22:28:20 +0100

    :sparkles: Intro new feature / wip ...

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import About from "@/components/About";
+import Content from "@/components/Content";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <About />
+      <Content />
     </div>
   );
 };
