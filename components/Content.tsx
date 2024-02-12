7e918c5a94e125ffb5b494042f8684c649a11320
Nnamdi Okeke <onnamdialex@gmail.com>
2024-02-12 19:13:35 +0100

    :construction: Work in progress

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Image from "next/image";
+import about1 from "../public/about1.png";
+import styles from "../styles/content.module.css";
 
 const Content = () => {
   return (
-    <div>
-      <div>hey</div>
+    <div className={styles.container}>
+      <div>
+        <Image src={about1} alt="abt" />
+        <p>
+          lorem ipsum yes yeshsh scbcnj hjnch <br />
+          jbchcc cchbchcc chccbcdc,b u{" "}
+        </p>
+      </div>
     </div>
   );
 };
