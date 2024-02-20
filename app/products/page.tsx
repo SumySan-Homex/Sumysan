6167b6b7be776f6e32eda86ffa662e30adb9711f
Nnamdi Okeke <onnamdialex@gmail.com>
2024-02-20 14:17:41 +0100

    :lipstick: Update style files

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import Image from "next/image";
 import styles from "../../styles/page.module.css";
+import pro1 from "../../public/pro1.png";
+import pro2 from "../../public/pro2.png";
+import pro3 from "../../public/pro3.png";
+import pro4 from "../../public/pro4.png";
 
 const Products = () => {
   return (
-    <div>
-      <div className={styles.productContainer}>products</div>
+    <div className={styles.productContainer}>
+      <div>
+        <Image src={pro1} alt="image" />
+        <Image src={pro2} alt="image" />
+        <Image src={pro3} alt="image" />
+        <Image src={pro4} alt="image" />
+      </div>
     </div>
   );
 };
