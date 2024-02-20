7aef84ca439e15ecba26a3064e74ca9aca78de8c
Nnamdi Okeke <onnamdialex@gmail.com>
2024-02-20 14:05:44 +0100

    :art: New images for the product pages introduced

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "../../styles/page.module.css";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div>
+      <div className={styles.productContainer}>products</div>
+    </div>
+  );
 };
 
 export default Products;
