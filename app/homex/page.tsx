1f9d3c625f9ceaf62c589d2d163674d8c8c917f0
Nnamdi Okeke <onnamdialex@gmail.com>
2024-02-21 16:39:04 +0100

    :rocket: Deployment of the homex page

--- app/homex/page.tsx
+++ app/homex/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import homex1 from "../../public/homex1.png";
 import homex2 from "../../public/homex2.png";
+import homex3 from "../../public/homex3.png";
 import styles from "../../styles/page.module.css";
 
 const Homex = () => {
@@ -35,6 +37,26 @@ const Homex = () => {
           waves quant ICON.
         </p>
         <Image className={styles.homexRoomImage} src={homex2} alt="image" />
+      </div>
+
+      <div className={styles.homexContent3}>
+        <Image src={homex3} alt="image" />
+
+        <p className={styles.homexParagraphText3}>
+          Crypto ipsum bitcoin ethereum dogecoin litecoin. Livepeer
+          <br />
+          polygon crypto PancakeSwap uniswap binance cardano
+          <br />
+          maker hive avalanche. Horizen chiliz ren neo USD quant
+          <br />
+          waves quant ICON.
+        </p>
+      </div>
+
+      <div className={styles.buttonContainer}>
+        <button className={styles.buttonLink}>
+          <Link href="/homex">visit site</Link>
+        </button>
       </div>
     </div>
   );
