e73106cff93159dd29973f75a8882b53be0fb8c3
Nnamdi Okeke <onnamdialex@gmail.com>
2024-02-26 14:08:10 +0100

    :lipstick: Add new styles for the career page. wip ...

--- app/careerOpportunities/page.tsx
+++ app/careerOpportunities/page.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Image from "next/image";
+import co1 from "../../public/co1.png";
+import styles from "../../styles/page.module.css";
 
 const CareerOpportunities = () => {
-  return <div>CareerOpportunities</div>;
+  return (
+    <div className={styles.coContainer}>
+      <div>
+        <div>
+          <h3>Here are Career Opportunities in Sumysan</h3>
+          <p>
+            Crypto ipsum bitcoin ethereum dogecoin litecoin. Livepeer
+            <br />
+            polygon crypto PancakeSwap uniswap binance cardano
+            <br />
+            maker hive avalanche. Horizen chiliz ren neo USD quant
+            <br />
+            waves quant ICON.
+          </p>
+        </div>
+
+        <Image src={co1} alt="image" />
+      </div>
+    </div>
+  );
 };
 
 export default CareerOpportunities;
