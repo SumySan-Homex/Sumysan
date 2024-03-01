2b263d960a8750b88ff56a1bed39b466537ce23d
Nnamdi Okeke <onnamdialex@gmail.com>
2024-03-01 18:12:18 +0100

    :zap: Improve performance of the FAQ page. styling incoming

--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import styles from "../../styles/page.module.css";
 
 const Faq = () => {
-  return <div>Faq</div>;
+  //state to manage the toggling of the dropdown
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  //function to manage the toggle button
+  const handleToggle = () => {
+    setDropdownOpen(!dropdownOpen);
+    // (prevDropdownOpen: any) => !prevDropdownOpen
+  };
+  return (
+    <div className={styles.faqContainer}>
+      <div className={styles.faqContents}>
+        <h3 className={styles.faqHeader}>Frequently Asked Questions</h3>
+
+        <div className={styles.faqDropdown}>
+          <p>what is sumysan ?</p>
+
+          {/* for dropdown to appear when dropdownOpen state is true */}
+          {dropdownOpen && (
+            <p>
+              Sumysan is a prop tech company into real estate aquisition and
+              <br />
+              sorts.
+            </p>
+          )}
+
+          <button
+            type="button"
+            onClick={handleToggle}
+            className={styles.faqToggleBtn}
+          >
+            {dropdownOpen ? "x" : "˅"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Faq;
