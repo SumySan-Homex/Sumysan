73cd42123648fb4a5f3bf4ea39ad4fb55f2da053
Nnamdi Okeke <onnamdialex@gmail.com>
2024-03-01 14:52:03 +0100

    :rocket: Deploy blog design page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styles from "../../styles/page.module.css";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className={styles.blogContainer}>
+      <div className={styles.blogContents}>
+        <h3 className={styles.blogHeader}>Blog</h3>
+        <p className={styles.blogParagraph}>
+          Crypto ipsum bitcoin ethereum dogecoin litecoin. Livepeer
+          <br />
+          polygon crypto PancakeSwap uniswap binance cardano maker hive
+          avalanche. Horizen chiliz ren neo
+          <br />
+          USD quant waves quant ICON.
+        </p>
+        <h3 className={styles.blogHeader1}>Top Categories</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
