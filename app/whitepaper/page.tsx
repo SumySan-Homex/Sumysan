d40ae856ae75d08ef4daa913d469274b14ae9d20
Nnamdi Okeke <onnamdialex@gmail.com>
2024-03-01 14:28:07 +0100

    :rocket: Whitepaper design available done

--- app/whitepaper/page.tsx
+++ app/whitepaper/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "../../styles/page.module.css";
 
 const Whitepaper = () => {
-  return <div>Whitepaper</div>;
+  return (
+    <div className={styles.wpContainer}>
+      <div className={styles.wpTextContainer}>
+        <h3 className={styles.wpText}>
+          Here are Career Opportunities
+          <br />
+          in Sumysan
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default Whitepaper;
