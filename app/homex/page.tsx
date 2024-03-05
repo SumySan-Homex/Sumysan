4e258e3a80b1737456b4d1f118366b54750da299
Nnamdi Okeke <onnamdialex@gmail.com>
2024-03-05 16:14:38 +0100

    :art: Improve structure / wip ...

--- app/homex/page.tsx
+++ app/homex/page.tsx
@@ -9,71 +9,17 @@ import styles from "../../styles/page.module.css";
 const Homex = () => {
   return (
     <div className={styles.homexContainer}>
-      <div className={styles.homexContent1}>
-        <Image src={homex1} alt="image" width={220} height={150} />
-
-        <div className={styles.homexInnerContent}>
-          <h3 className={styles.homexTextContent}>Homex</h3>
-          <p className={styles.homexParagraphText}>
-            Crypto ipsum bitcoin ethereum dogecoin litecoin. Livepeer
-            <br />
-            polygon crypto PancakeSwap uniswap binance cardano
-            <br />
-            maker hive avalanche. Horizen chiliz ren neo USD quant
-            <br />
-            waves quant ICON.
-          </p>
-        </div>
-      </div>
-
-      <div className={styles.homexContent2}>
-        <p className={styles.homexParagraphText2}>
-          Crypto ipsum bitcoin ethereum dogecoin litecoin. Livepeer
-          <br />
-          polygon crypto PancakeSwap uniswap binance cardano
-          <br />
-          maker hive avalanche. Horizen chiliz ren neo USD quant
-          <br />
-          waves quant ICON.
-        </p>
-        <Image className={styles.homexRoomImage} src={homex2} alt="image" />
-      </div>
-
-      <div className={styles.homexContent3}>
-        <Image src={homex3} alt="image" />
-
-        <p className={styles.homexParagraphText3}>
-          Crypto ipsum bitcoin ethereum dogecoin litecoin. Livepeer
-          <br />
-          polygon crypto PancakeSwap uniswap binance cardano
-          <br />
-          maker hive avalanche. Horizen chiliz ren neo USD quant
-          <br />
-          waves quant ICON.
-        </p>
-      </div>
-
-      <div className={styles.buttonContainer}>
-        <button className={styles.buttonLink}>
-          <Link href="">visit site</Link>
-        </button>
-      </div>
-
-      <div className={styles.homexContent4}>
-        <h3 className={styles.homexTitleContent}>
-          Find your home according to your budget.
-        </h3>
-        <p className={styles.homexParagraphText4}>
-          Our friendly home recommendation AI that helps you find homes that
-          suit your needs and budgets.
-        </p>
-        <button></button>
-      </div>
-
-      <div className={styles.buttonContainer}>
-        <button className={styles.buttonLink}>
-          <Link href="">Explore</Link>
-        </button>
+      <div className={styles.homexContents}>
+        <h1 className={styles.homexHeader}>
+          Finding Your Dream Home With People You Trust
+        </h1>
+        <Image
+          className={styles.homexLogo}
+          src={homex1}
+          alt="homexImage"
+          width={400}
+          height={300}
+        />
       </div>
     </div>
   );
