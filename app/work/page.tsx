0b8d30445f0aad78902cbf9e18cec7e818ac5c0c
Nnamdi Okeke <onnamdialex@gmail.com>
2024-03-12 15:50:21 +0100

    :construction: Wip

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -8,55 +8,12 @@ import styles from "../../styles/page.module.css";
 
 const Work = () => {
   return (
-    <div className={styles.homexContainer}>
-      <div className={styles.homexContent1}>
-        <Image src={work1} alt="image" width={220} height={150} />
-
-        <div className={styles.homexInnerContent}>
-          <h3 className={styles.homexTextContent}>Work for me</h3>
-          <p className={styles.homexParagraphText}>
-            Crypto ipsum bitcoin ethereum dogecoin litecoin. Livepeer
-            <br />
-            polygon crypto PancakeSwap uniswap binance cardano
-            <br />
-            maker hive avalanche. Horizen chiliz ren neo USD quant
-            <br />
-            waves quant ICON.
-          </p>
-        </div>
-      </div>
-
-      <div className={styles.homexContent2}>
-        <p className={styles.homexParagraphText2}>
-          Crypto ipsum bitcoin ethereum dogecoin litecoin. Livepeer
-          <br />
-          polygon crypto PancakeSwap uniswap binance cardano
-          <br />
-          maker hive avalanche. Horizen chiliz ren neo USD quant
-          <br />
-          waves quant ICON.
-        </p>
-        <Image className={styles.homexRoomImage} src={work2} alt="image" />
-      </div>
-
-      <div className={styles.homexContent3}>
-        <Image src={work3} alt="image" />
-
-        <p className={styles.homexParagraphText3}>
-          Crypto ipsum bitcoin ethereum dogecoin litecoin. Livepeer
-          <br />
-          polygon crypto PancakeSwap uniswap binance cardano
-          <br />
-          maker hive avalanche. Horizen chiliz ren neo USD quant
-          <br />
-          waves quant ICON.
-        </p>
-      </div>
-
-      <div className={styles.buttonContainer}>
-        <button className={styles.buttonLink}>
-          <Link href="">visit site</Link>
-        </button>
+    <div className={styles.workContainer}>
+      <div className={styles.workContent1}>
+        <h4>
+          Charting Your Career Path: <br />
+          Work for Us at SUMYSAN!
+        </h4>
       </div>
     </div>
   );
